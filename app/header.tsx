e983fabe2b95c381f39acba01011c809819e3a1f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-03 16:45:09 +0000

    Add Logo on Header

--- app/header.tsx
+++ app/header.tsx
@@ -6,6 +6,7 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
 
 export const Header = () => {
@@ -13,6 +14,7 @@ export const Header = () => {
     <div className="relative z-10 border-b py-4 bg-gray-50">
       <div className="items-center container mx-auto justify-between flex">
         <Link href="/" className="flex gap-2 items-center text-xl text-black">
+          <Image src="/logo.png" width="50" height="50" alt="file drive logo" />
           File Drive
         </Link>
 
